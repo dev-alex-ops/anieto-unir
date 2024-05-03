7e5fa2da97e3fbfb64a7ef579b221e9e60b647b3
Alejandro Salas <devalexops@gmail.com>
2024-05-03 11:06:38 +0200

    Added endpoints and service tests

--- test/rest/api_test.py
+++ test/rest/api_test.py
@@ -2,6 +2,7 @@ import http.client
 import os
 import unittest
 from urllib.request import urlopen
+from urllib.error import HTTPError
 
 import pytest
 
@@ -25,6 +26,39 @@ class TestApi(unittest.TestCase):
             response.read().decode(), "3", "ERROR ADD"
         )
 
+    def test_api_multiply(self):
+        url = f"{BASE_URL}/calc/multiply/2/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "4", "ERROR MULTIPLY"
+        )
+
+    def test_api_divide(self):
+        url = f"{BASE_URL}/calc/divide/2/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "1.0", "ERROR DIVIDE"
+        )
+
+
+    def test_api_divide_0(self):
+        url = f"{BASE_URL}/calc/divide/4/0"
+        try:
+            response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        except HTTPError as e:
+            self.assertEqual(
+                e.code, http.client.NOT_ACCEPTABLE, f"Error en la petición API a {url}"
+            )
+            self.assertEqual(
+                e.read().decode(), "Error: No se puede dividir por 0", "ERROR DIVIDE"
+            )
+
     def test_api_sqrt(self):
         url = f"{BASE_URL_MOCK}/calc/sqrt/64"
         response = urlopen(url, timeout=DEFAULT_TIMEOUT)
@@ -34,6 +68,7 @@ class TestApi(unittest.TestCase):
         self.assertEqual(
             response.read().decode(), "8", "ERROR SQRT"
         )
+
 
 if __name__ == "__main__":  # pragma: no cover
     unittest.main()
